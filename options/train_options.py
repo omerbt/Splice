675fd8f2adf48b4d3e10408f18c66e1190e3c1c9
omerbartal <omer234@gmail.com>
2021-08-17 15:36:43 +0300

    set display_id=0

--- options/train_options.py
+++ options/train_options.py
@@ -13,7 +13,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--log_images_freq', type=int, default=400, help='frequency of logging generated images to wandb')
         parser.add_argument('--display_freq', type=int, default=400, help='frequency of showing training results on screen')
         parser.add_argument('--display_ncols', type=int, default=4, help='if positive, display all images in a single visdom web panel with certain number of images per row.')
-        parser.add_argument('--display_id', type=int, default=None, help='window id of the web display. Default is random window id')
+        parser.add_argument('--display_id', type=int, default=0, help='window id of the web display. Default is random window id')
         parser.add_argument('--display_server', type=str, default="http://localhost", help='visdom server of the web display')
         parser.add_argument('--display_env', type=str, default='main', help='visdom display environment name (default is "main")')
         parser.add_argument('--display_port', type=int, default=8097, help='visdom port of the web display')
