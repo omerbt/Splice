7750bf9289886552c52a2ab3557550bad72860a8
omerbartal <omer234@gmail.com>
2021-08-29 17:34:53 +0300

    noise exp

--- data/singleimage_dataset.py
+++ data/singleimage_dataset.py
@@ -110,8 +110,8 @@ class SingleImageDataset(BaseDataset):
                      'flip': random.random() > 0.5}
 
             transform_A = get_transform(self.opt, params=param, method=Image.BILINEAR)
-            A = transform_A(A_img)
-
+            # A = transform_A(A_img)
+            A = A_img
             param = {'scale_factor': self.zoom_levels_B[index],
                      'patch_index': self.patch_indices_B[index],
                      'flip': random.random() > 0.5}
