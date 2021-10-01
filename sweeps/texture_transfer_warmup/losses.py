b0f39a47e66460237d57b802692720773638a18c
tnarek99 <narektumanyan99@gmail.com>
2021-10-01 19:36:36 +0300

    adding warmup code

---
+++ sweeps/texture_transfer_warmup/losses.py
@@ -0,0 +1,186 @@
+from torchvision.transforms import Resize
+from torchvision import transforms
+import torch
+import torch.nn.functional as F
+from models.extractor import VitExtractor
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class LossG(torch.nn.Module):
+
+    def __init__(self, A_img, B_img, cfg):
+        super().__init__()
+
+        self.cfg = cfg
+        self.extractor = VitExtractor(model_name=cfg['dino_model_name'], device=device)
+
+        imagenet_norm = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        resize_transform = Resize(cfg['dino_global_patch_size'], max_size=480)
+
+        self.global_transform = transforms.Compose([resize_transform,
+                                                    transforms.Normalize((-1, -1, -1), (2, 2, 2)),  # [-1, 1] -> [0, 1]
+                                                    imagenet_norm
+                                                    ])
+        self.local_transform = transforms.Compose([
+            transforms.Normalize((-1, -1, -1), (2, 2, 2)),  # [-1, 1] -> [0, 1]
+            imagenet_norm
+        ])
+
+        A = transforms.Compose([transforms.ToTensor(), resize_transform, imagenet_norm])(A_img).unsqueeze(0)
+        B = transforms.Compose([transforms.ToTensor(), resize_transform, imagenet_norm])(B_img).unsqueeze(0)
+        self.target_global_ssim = self.extractor.get_keys_self_sim_from_input(A.to(device), layer_num=11).detach()
+        self.target_global_cls_token = self.extractor.get_feature_from_input(B.to(device))[-1][0, 0, :].detach()
+        self.B = B.to(device)
+        self.register_buffer("step", torch.zeros(1))
+        self.init_lambda_config()
+
+    def init_lambda_config(self):
+        self.lambdas = dict(
+            lambda_ssim=0,
+            lambda_cls=self.cfg['lambda_cls'],
+            lambda_patch_ssim=0,
+            lambda_global_ssim=0,
+            lambda_global_cls_cropped=self.cfg['lambda_global_cls_cropped'],
+            lambda_global_cls=0,
+            lambda_local_cls=0,
+            lambda_identity=0,
+            lambda_crops_cls=0
+        )
+
+    def update_lambda_config(self):
+        if self.step == self.cfg['cls_warmup']:
+            self.lambdas['lambda_ssim'] = self.cfg['lambda_ssim']
+            self.lambdas['lambda_patch_ssim'] = self.cfg['lambda_patch_ssim']
+            self.lambdas['lambda_global_ssim'] = self.cfg['lambda_global_ssim']
+            self.lambdas['loss_global_cls_cropped'] = self.cfg['lambda_global_cls_cropped']
+            self.lambdas['lambda_global_cls'] = self.cfg['lambda_global_cls']
+            self.lambdas['lambda_local_cls'] = self.cfg['lambda_local_cls']
+            self.lambdas['lambda_identity'] = self.cfg['lambda_identity']
+            self.lambdas['lambda_crops_cls'] = self.cfg['lambda_crops_cls']
+
+    def update_step(self):
+        self.step += 1
+        self.update_lambda_config()
+
+    def forward(self, outputs, inputs):
+        losses = {}
+
+        loss_G = 0
+
+        losses['loss_ssim'] = self.calculate_ssim_loss(outputs['x'])
+        losses['loss_cls'] = self.calculate_global_cls_loss(outputs['x'], self.lambdas['lambda_cls'])
+        losses['loss_patch_ssim'] = self.calculate_local_ssim_loss(outputs['x_local'], inputs['A_local'])
+        losses['loss_global_ssim'] = self.calculate_global_ssim_loss(outputs['x_global'], inputs['A_global'])
+        losses['loss_global_cls_cropped'] = self.calculate_global_cls_cropped_loss(outputs['x_global'], inputs['B_global'])
+        losses['loss_global_cls'] = self.calculate_global_cls_loss(outputs['x_global'], self.lambdas['lambda_global_cls'])
+        losses['loss_local_cls'] = self.calculate_local_cls_loss(outputs['x_local'])
+        losses['loss_crops_cls'] = self.calculate_crops_cls_loss(outputs['x'])
+        losses['loss_idt_B'] = F.l1_loss(outputs['y_local'], inputs['B_local'])
+
+        loss_G += losses['loss_ssim'] * self.lambdas['lambda_ssim']
+        loss_G += losses['loss_cls'] * self.lambdas['lambda_cls']
+        loss_G += losses['loss_patch_ssim'] * self.lambdas['lambda_patch_ssim']
+        loss_G += losses['loss_global_ssim'] * self.lambdas['lambda_global_ssim']
+        loss_G += losses['loss_global_cls_cropped'] * self.lambdas['lambda_global_cls_cropped']
+        loss_G += losses['loss_global_cls'] * self.lambdas['lambda_global_cls']
+        loss_G += losses['loss_local_cls'] * self.lambdas['lambda_local_cls']
+        loss_G += losses['loss_idt_B'] * self.lambdas['lambda_identity']
+
+        loss_G += losses['loss_crops_cls'] * self.lambdas['lambda_crops_cls']
+
+        losses['loss'] = loss_G
+
+        self.update_step()
+        return losses
+
+    def calculate_ssim_loss(self, output):
+        if self.lambdas['lambda_ssim'] == 0:
+            return 0
+        x = self.global_transform(output)
+        ssim = self.extractor.get_keys_self_sim_from_input(x, layer_num=11)
+        loss = F.mse_loss(ssim, self.target_global_ssim)
+        return loss
+
+    def calculate_local_ssim_loss(self, outputs, inputs):
+        if self.lambdas['lambda_patch_ssim'] == 0:
+            return 0
+        loss = 0.0
+        for a, b in zip(inputs, outputs):  # avoid memory limitations
+            a = self.local_transform(a)
+            b = self.local_transform(b)
+            target_keys_self_sim = self.extractor.get_keys_self_sim_from_input(a.unsqueeze(0),
+                                                                               layer_num=11).detach()
+            keys_ssim = self.extractor.get_keys_self_sim_from_input(b.unsqueeze(0), layer_num=11)
+            loss += F.mse_loss(keys_ssim, target_keys_self_sim)
+        return loss
+
+    def calculate_global_ssim_loss(self, outputs, inputs):
+        if self.lambdas['lambda_global_ssim'] == 0:
+            return 0
+        loss = 0.0
+        for a, b in zip(inputs, outputs):  # avoid memory limitations
+            a = self.global_transform(a)
+            b = self.global_transform(b)
+            target_keys_self_sim = self.extractor.get_keys_self_sim_from_input(a.unsqueeze(0),
+                                                                               layer_num=11).detach()
+            keys_ssim = self.extractor.get_keys_self_sim_from_input(b.unsqueeze(0), layer_num=11)
+            loss += F.mse_loss(keys_ssim, target_keys_self_sim)
+        return loss
+
+    def calculate_global_cls_cropped_loss(self, outputs, inputs):
+        if self.cfg['lambda_global_cls_cropped'] == 0:
+            return 0
+        loss = 0.0
+        for a, b in zip(inputs, outputs):
+            a = self.global_transform(a)
+            b = self.global_transform(b)
+            target_cls_token = self.extractor.get_feature_from_input(a.unsqueeze(0))[-1][0, 0, :]
+            cls_token = self.extractor.get_feature_from_input(b.unsqueeze(0))[-1][0, 0, :]
+            loss += F.mse_loss(cls_token, target_cls_token)
+        return loss
+
+    def calculate_global_cls_loss(self, outputs, loss_lambda):
+        if loss_lambda == 0:
+            return 0
+        loss = 0.0
+        for a in outputs:  # avoid memory limitations
+            a = self.global_transform(a).unsqueeze(0)
+            cls_token = self.extractor.get_feature_from_input(a)[-1][0, 0, :]
+            loss += F.mse_loss(cls_token, self.target_global_cls_token)
+        return loss
+
+    def calculate_local_cls_loss(self, outputs):
+        if self.lambdas['lambda_local_cls'] == 0:
+            return 0
+        loss = 0.0
+        for a in outputs:
+            a = self.local_transform(a).unsqueeze(0)
+            cls_token = self.extractor.get_feature_from_input(a)[-1][0, 0, :]
+            loss += F.mse_loss(cls_token, self.target_global_cls_token)
+        return loss
+
+    def calculate_crops_cls_loss(self, output):
+        if self.lambdas['lambda_crops_cls'] == 0:
+            return 0
+        x = self.global_transform(output)
+        loss = 0.0
+        n_crops = 4
+        for _ in range(n_crops):
+            a = transforms.RandomCrop(self.lambdas['local_crops_crop_size'])(x)
+            b = transforms.RandomCrop(self.lambdas['local_crops_crop_size'])(self.B)
+            cls_token = self.extractor.get_feature_from_input(a)[-1][0, 0, :]
+            target_cls_token = self.extractor.get_feature_from_input(b)[-1][0, 0, :]
+            loss += F.mse_loss(cls_token, target_cls_token)
+        return loss
+
+    def calculate_local_ssim(self, outputs, inputs):
+        loss = 0.0
+        for a, b in zip(inputs, outputs):  # avoid memory limitations
+            a = self.local_transform(a)
+            b = self.local_transform(b)
+            target_keys_self_sim = self.extractor.get_keys_self_sim_from_input(a.unsqueeze(0),
+                                                                               layer_num=11).detach()
+            keys_ssim = self.extractor.get_keys_self_sim_from_input(b.unsqueeze(0), layer_num=11)
+            loss += F.mse_loss(keys_ssim, target_keys_self_sim)
+        return loss
---
+++ sweeps/texture_transfer_warmup/main.py
@@ -0,0 +1,143 @@
+import sys
+
+sys.path.insert(0, "/home/labs/waic/narekt/projects/texture-mapping")
+
+
+import logging
+# import hydra
+# from hydra import utils
+import torch
+import wandb
+import numpy as np
+import random
+import os
+from data.Dataset import SingleImageDataset
+from models.model import Model
+from sweeps.texture_transfer_warmup.losses import LossG
+from util.util import tensor2im, get_scheduler, get_optimizer
+# from omegaconf import OmegaConf
+import yaml
+
+log = logging.getLogger(__name__)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def validate_cfg(cfg):
+    if (
+            (cfg['scheduler_policy'] == 'cosine_dino' and (
+                    cfg['base_lr'] < 0 or cfg['final_lr'] < 0 or cfg['warmup_epochs_prop'] < 0 or cfg['start_warmup_lr'] < 0))
+            or
+            (cfg['scheduler_policy'] != 'cosine_dino' and (
+            cfg['base_lr'] > 0 or cfg['final_lr'] > 0 or cfg['warmup_epochs_prop'] > 0 or cfg['start_warmup_lr'] > 0))
+    ):
+        return False
+
+    return True
+
+
+def make_scheduler_step(optimizer, scheduler, iter, cfg):
+    if cfg['scheduler_policy'] == 'cosine_dino':
+        optimizer.param_groups[0]['lr'] = scheduler[iter]
+    else:
+        scheduler.step()
+
+
+def overfit_to_content(dataset, model):
+    epochs = 1500
+    lr = 0.01
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    content_img = dataset[0]['A'].to(device)
+
+    print("overfitting G to content...")
+    for _ in range(epochs):
+        optimizer.zero_grad()
+        pred = model(content_img)
+        loss = torch.nn.MSELoss()(pred, content_img)
+        loss.backward()
+        optimizer.step()
+
+    return model.parameters()
+
+
+# @hydra.main(config_path='conf/default', config_name='config')
+def train_model():
+    with open("sweeps/texture_transfer_warmup/default_config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+    wandb.init(project='semantic-texture-transfer', entity='vit-vis', config=config)
+    cfg = wandb.config
+
+    is_cfg_valid = validate_cfg(cfg)
+    if not is_cfg_valid:
+        print("invalid config, aborting the run...")
+        # run = api.run(f'vit-vis/semantic-texture-transfer/{wandb.run.id}')
+        # run.delete()
+        return
+
+    # set seed
+    if cfg['seed'] == -1:
+        seed = np.random.randint(2 ** 32)
+        cfg['seed'] = seed
+    random.seed(cfg['seed'])
+    np.random.seed(cfg['seed'])
+    torch.manual_seed(cfg['seed'])
+    log.info(f'seed: {cfg["seed"]}')
+
+    # create dataset, loader
+    dataset = SingleImageDataset(cfg)
+
+    # define model
+    model = Model(cfg)
+
+    # define loss function
+    criterion = LossG(dataset.A_img, dataset.B_img, cfg)
+
+    # define optimizer, scheduler
+    optimizer = get_optimizer(cfg, model.netG.parameters())
+
+    scheduler = get_scheduler(optimizer,
+                              cfg,
+                              n_epochs=cfg['n_epochs'],
+                              n_epochs_decay=cfg['scheduler_n_epochs_decay'],
+                              lr_decay_iters=cfg['scheduler_lr_decay_iters'])
+
+    overfit_to_content(dataset, model.netG)
+    for epoch in range(1, cfg['n_epochs'] + 1):
+        inputs = dataset[0]
+        for key in inputs:
+            inputs[key] = inputs[key].to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        losses = criterion(outputs, inputs)
+        loss_G = losses['loss']
+        loss_G.backward()
+        optimizer.step()
+
+        # log gradient total norm
+        grad_norm = 0.0
+        for p in list(filter(lambda p: p.grad is not None, model.parameters())):
+            grad_norm += p.grad.data.norm(2).item()
+        wandb.log({'grad_norm': grad_norm})
+
+        # log losses
+        wandb.log(losses)
+
+        # update learning rate
+        make_scheduler_step(optimizer, scheduler, epoch, cfg)
+        lr = optimizer.param_groups[0]['lr']
+        log.info('learning rate = %.7f' % lr)
+        wandb.log({"lr": lr, "epoch": epoch})
+
+        # log current generated entire image to wandb
+        if epoch % cfg['log_images_freq'] == 0:
+            print(f'epoch: {epoch}')
+            model.netG.eval()
+            img_A = dataset.get_A().to(device)
+            with torch.no_grad():
+                output = model.netG(img_A)
+            image_numpy = tensor2im(output)
+            wandb.log({"img": [wandb.Image(image_numpy)]})
+            model.netG.train()
+
+
+if __name__ == '__main__':
+    train_model()
