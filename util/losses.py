0b1a4273994eb909c4f355c18ee747a6227f1085
omerbartal <omer234@gmail.com>
2021-10-01 16:38:38 +0300

    match CLS of crops from texture

--- util/losses.py
+++ util/losses.py
@@ -32,7 +32,8 @@ class LossG(torch.nn.Module):
 
         self.global_B_patches = Global_crops(n_crops=cfg['global_B_crops_n_crops'],
                                              min_cover=cfg['global_B_crops_min_cover'],
-                                             last_transform=transforms.Compose([transforms.ToTensor(),
+                                             last_transform=transforms.Compose([resize_transform,
+                                                                                transforms.ToTensor(),
                                                                                 imagenet_norm]))
 
         B = transforms.Compose([transforms.ToTensor(), resize_transform, imagenet_norm])(B_img).unsqueeze(0)
@@ -84,7 +85,7 @@ class LossG(torch.nn.Module):
         loss = 0.0
         for a, b in zip(inputs, outputs):  # avoid memory limitations
             a = self.global_transform(a).unsqueeze(0)
-            b = self.global_transform(b).unsqueeze(0).to(device)
+            b = b.unsqueeze(0).to(device)
             cls_token = self.extractor.get_feature_from_input(a)[-1][0, 0, :]
             target_cls_token = self.extractor.get_feature_from_input(b)[-1][0, 0, :]
             loss += F.mse_loss(cls_token, target_cls_token)
