36569f9c824e9a102d7dda352b86fc5e57a2f72d
tnarek99 <narektumanyan99@gmail.com>
2021-10-11 15:11:07 +0300

    bilinear ups

--- models/networks.py
+++ models/networks.py
@@ -54,5 +54,5 @@ def init_net(net, init_type='normal', init_gain=0.02, debug=False, initialize_we
 
 
 def define_G(init_type='normal', init_gain=0.02):
-    net = skip(need_sigmoid=True)
+    net = skip(need_sigmoid=True, upsample_mode='bilinear')
     return init_net(net, init_type, init_gain, initialize_weights=True)
