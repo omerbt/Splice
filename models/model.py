82982889a2bb4f7de1c1713716e94ef121d0417a
omerbartal <omer234@gmail.com>
2021-10-31 12:03:14 +0200

    exp

--- models/model.py
+++ models/model.py
@@ -9,17 +9,20 @@ class Model(torch.nn.Module):
         self.netG = networks.define_G(cfg['init_type'], cfg['init_gain'], cfg['upsample_mode'],
                                       cfg['downsample_mode']).to(device)
         self.cfg = cfg
+        self.register_buffer('noise', torch.tensor(0))
 
     def forward(self, input):
         outputs = {}
         # global patches from structure image
         if self.cfg['lambda_global_cls'] + self.cfg['lambda_global_ssim'] > 0:
-            outputs['x_global'] = self.netG(input['A_global'])
+            outputs['x_global'] = self.netG(
+                input['A_global'] + self.noise.expand_dims(*input['A_global'].shape).normal_() * 10)
 
         # entire structure image
         if self.cfg['lambda_entire_ssim'] + self.cfg['lambda_entire_cls'] > 0 and input['step'] % self.cfg[
             'entire_A_every'] == 0:
-            outputs['x_entire'] = self.netG(input['A'])
+            outputs['x_entire'] = self.netG(input['A'] +
+                                            self.noise.expand_dims(*input['A'].shape).normal_() * 10)
 
         # global patches from texture image
         outputs['y_global'] = self.netG(input['B_global'])
