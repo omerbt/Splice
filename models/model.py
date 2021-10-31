ed6a0df675414f4362301f3e86e3232c821c5809
omerbartal <omer234@gmail.com>
2021-10-31 12:07:16 +0200

    exp

--- models/model.py
+++ models/model.py
@@ -16,13 +16,13 @@ class Model(torch.nn.Module):
         # global patches from structure image
         if self.cfg['lambda_global_cls'] + self.cfg['lambda_global_ssim'] > 0:
             outputs['x_global'] = self.netG(
-                input['A_global'] + self.noise.expand(*input['A_global'].shape).normal_() * 10)
+                input['A_global'] + input['A_global'].clone().detach().normal_() * 10)
 
         # entire structure image
         if self.cfg['lambda_entire_ssim'] + self.cfg['lambda_entire_cls'] > 0 and input['step'] % self.cfg[
             'entire_A_every'] == 0:
             outputs['x_entire'] = self.netG(input['A'] +
-                                            self.noise.expand(*input['A'].shape).normal_() * 10)
+                                            input['A'].clone().detach().normal_() * 10)
 
         # global patches from texture image
         outputs['y_global'] = self.netG(input['B_global'])
